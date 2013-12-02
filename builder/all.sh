04ff464cd0b24a83164e66487b59066eab31a3c1
Brad Rydzewski <brad.rydzewski@gmail.com>
2013-12-02 16:19:24 -0700

    removed sudo from build scripts

--- builder/all.sh
+++ builder/all.sh
@@ -4,72 +4,69 @@ set -e
 ##################################################################################
 # Base Image
 
-sudo docker build -rm -t bradrydzewski/base builder/base/
+#docker build -rm -t bradrydzewski/base builder/base/
 
 ##################################################################################
 # Dart Images
 
-sudo docker build -rm -t bradrydzewski/dart     builder/dart/dart_stable/
-sudo docker build -rm -t bradrydzewski/dart:dev builder/dart/dart_dev/
+docker build -rm -t bradrydzewski/dart     builder/dart/dart_stable/
+docker build -rm -t bradrydzewski/dart:dev builder/dart/dart_dev/
 
 ##################################################################################
 # Go Images
 
-sudo docker build -rm -t bradrydzewski/go:1.2 builder/golang/go_1.2/
-sudo docker build -rm -t bradrydzewski/go:1.1 builder/golang/go_1.1/
-#sudo docker build -rm -t bradrydzewski/go:1.0 builder/golang/go_1.0/
+docker build -rm -t bradrydzewski/go:1.2 builder/golang/go_1.2/
+docker build -rm -t bradrydzewski/go:1.1 builder/golang/go_1.1/
+docker build -rm -t bradrydzewski/go:1.0 builder/golang/go_1.0/
 
 ##################################################################################
 # Haskell Images
 
-sudo docker build -rm -t bradrydzewski/haskell:7.4 builder/haskell/haskell_7.4/
+docker build -rm -t bradrydzewski/haskell:7.4 builder/haskell/haskell_7.4/
 
 ##################################################################################
 # Node Images
 
-sudo docker build -rm -t bradrydzewski/node      builder/node/node/
-sudo docker build -rm -t bradrydzewski/node:0.10 builder/node/node_0.10/
-sudo docker build -rm -t bradrydzewski/node:0.8  builder/node/node_0.8/
-#sudo docker build -rm -t bradrydzewski/node:0.6  builder/node/node_0.6/
+docker build -rm -t bradrydzewski/node      builder/node/node/
+docker build -rm -t bradrydzewski/node:0.10 builder/node/node_0.10/
+docker build -rm -t bradrydzewski/node:0.8  builder/node/node_0.8/
 
 ##################################################################################
 # PHP Images
 
-sudo docker build -rm -t bradrydzewski/php     builder/php/php
-sudo docker build -rm -t bradrydzewski/php:5.5 builder/php/php_5.5
-sudo docker build -rm -t bradrydzewski/php:5.4 builder/php/php_5.4
-#sudo docker build -rm -t bradrydzewski/php:5.3 builder/php/php_5.3
+docker build -rm -t bradrydzewski/php     builder/php/php
+docker build -rm -t bradrydzewski/php:5.5 builder/php/php_5.5
+docker build -rm -t bradrydzewski/php:5.4 builder/php/php_5.4
 
 ##################################################################################
 # Python Images
 
-sudo docker build -rm -t bradrydzewski/python      builder/python/python/
-#sudo docker build -rm -t bradrydzewski/python:2.6  builder/python/python_2.6/
-sudo docker build -rm -t bradrydzewski/python:2.7  builder/python/python_2.7/
-sudo docker build -rm -t bradrydzewski/python:3.2  builder/python/python_3.2/
-sudo docker build -rm -t bradrydzewski/python:3.3  builder/python/python_3.3/
-sudo docker build -rm -t bradrydzewski/python:pypy builder/python/pypy/
+docker build -rm -t bradrydzewski/python      builder/python/python/
+docker build -rm -t bradrydzewski/python:2.7  builder/python/python_2.7/
+docker build -rm -t bradrydzewski/python:3.2  builder/python/python_3.2/
+docker build -rm -t bradrydzewski/python:3.3  builder/python/python_3.3/
+docker build -rm -t bradrydzewski/python:pypy builder/python/pypy/
 
 ##################################################################################
 # Ruby Images
 
-sudo docker build -rm -t bradrydzewski/ruby        builder/ruby/ruby/
-sudo docker build -rm -t bradrydzewski/ruby:1.9.3  builder/ruby/ruby_1.9.3/
-sudo docker build -rm -t bradrydzewski/ruby:2.0.0  builder/ruby/ruby_2.0.0/
+docker build -rm -t bradrydzewski/ruby        builder/ruby/ruby/
+docker build -rm -t bradrydzewski/ruby:1.9.3  builder/ruby/ruby_1.9.3/
+docker build -rm -t bradrydzewski/ruby:2.0.0  builder/ruby/ruby_2.0.0/
 
 ##################################################################################
 # Java Images
 
-#sudo docker build -rm -t bradrydzewski/java:oraclejdk8  builder/java/java_oraclejdk7/
-#sudo docker build -rm -t bradrydzewski/java:oraclejdk7  builder/java/java_oraclejdk7/
-#sudo docker build -rm -t bradrydzewski/java:openjdk7  builder/java/java_openjdk7/
-#sudo docker build -rm -t bradrydzewski/java:openjdk6  builder/java/java_openjdk6/
+#docker build -rm -t bradrydzewski/java:oraclejdk8  builder/java/java_oraclejdk7/
+#docker build -rm -t bradrydzewski/java:oraclejdk7  builder/java/java_oraclejdk7/
+#docker build -rm -t bradrydzewski/java:openjdk7  builder/java/java_openjdk7/
+#docker build -rm -t bradrydzewski/java:openjdk6  builder/java/java_openjdk6/
 
 ##################################################################################
 # Scala Images
 
-sudo docker build -rm -t bradrydzewski/scala:2.10.3  builder/scala/scala_2.10.3/
-sudo docker build -rm -t bradrydzewski/scala:2.9.3   builder/scala/scala_2.9.3/
+docker build -rm -t bradrydzewski/scala:2.10.3  builder/scala/scala_2.10.3/
+docker build -rm -t bradrydzewski/scala:2.9.3   builder/scala/scala_2.9.3/
 
 ##################################################################################
 # Groovy Images
@@ -79,27 +76,27 @@ sudo docker build -rm -t bradrydzewski/scala:2.9.3   builder/scala/scala_2.9.3/
 ##################################################################################
 # Clojure Images
 
-sudo docker build -rm -t bradrydzewski/lein builder/clojure/lein/
+docker build -rm -t bradrydzewski/lein builder/clojure/lein/
 
 ##################################################################################
 # GCC Images
 
-sudo docker build -rm -t bradrydzewski/gcc:4.6  builder/gcc/gcc_4.6/
-sudo docker build -rm -t bradrydzewski/gcc:4.8  builder/gcc/gcc_4.8/
+docker build -rm -t bradrydzewski/gcc:4.6  builder/gcc/gcc_4.6/
+docker build -rm -t bradrydzewski/gcc:4.8  builder/gcc/gcc_4.8/
 
 ##################################################################################
 # Erlang Images
 
-sudo docker build -rm -t bradrydzewski/erlang builder/erlang/erlang/
-sudo docker build -rm -t bradrydzewski/erlang:R16B02 builder/erlang/erlang_R16B02/
-sudo docker build -rm -t bradrydzewski/erlang:R16B01 builder/erlang/erlang_R16B01/
-sudo docker build -rm -t bradrydzewski/erlang:R16B   builder/erlang/erlang_R16B/
-#sudo docker build -rm -t bradrydzewski/erlang:R15B03 builder/erlang/erlang_R15B03/
-#sudo docker build -rm -t bradrydzewski/erlang:R15B02 builder/erlang/erlang_R15B02/
-#sudo docker build -rm -t bradrydzewski/erlang:R15B01 builder/erlang/erlang_R15B01/
-#sudo docker build -rm -t bradrydzewski/erlang:R15B   builder/erlang/erlang_R15B/
-#sudo docker build -rm -t bradrydzewski/erlang:R14B04 builder/erlang/erlang_R14B04/
-#sudo docker build -rm -t bradrydzewski/erlang:R14B03 builder/erlang/erlang_R14B03/
-#sudo docker build -rm -t bradrydzewski/erlang:R14B02 builder/erlang/erlang_R14B02/
-#sudo docker build -rm -t bradrydzewski/erlang:R14B01 builder/erlang/erlang_R14B01/
-#sudo docker build -rm -t bradrydzewski/erlang:R14A   builder/erlang/erlang_R14A/
+docker build -rm -t bradrydzewski/erlang builder/erlang/erlang/
+docker build -rm -t bradrydzewski/erlang:R16B02 builder/erlang/erlang_R16B02/
+docker build -rm -t bradrydzewski/erlang:R16B01 builder/erlang/erlang_R16B01/
+docker build -rm -t bradrydzewski/erlang:R16B   builder/erlang/erlang_R16B/
+#docker build -rm -t bradrydzewski/erlang:R15B03 builder/erlang/erlang_R15B03/
+#docker build -rm -t bradrydzewski/erlang:R15B02 builder/erlang/erlang_R15B02/
+#docker build -rm -t bradrydzewski/erlang:R15B01 builder/erlang/erlang_R15B01/
+#docker build -rm -t bradrydzewski/erlang:R15B   builder/erlang/erlang_R15B/
+#docker build -rm -t bradrydzewski/erlang:R14B04 builder/erlang/erlang_R14B04/
+#docker build -rm -t bradrydzewski/erlang:R14B03 builder/erlang/erlang_R14B03/
+#docker build -rm -t bradrydzewski/erlang:R14B02 builder/erlang/erlang_R14B02/
+#docker build -rm -t bradrydzewski/erlang:R14B01 builder/erlang/erlang_R14B01/
+#docker build -rm -t bradrydzewski/erlang:R14A   builder/erlang/erlang_R14A/
--- builder/clean.sh
+++ builder/clean.sh
@@ -2,62 +2,62 @@
 set -e
 
 # remove clojure image
-sudo docker rmi bradrydzewski/lein
+docker rmi bradrydzewski/lein
 
 # remove dart image
-sudo docker rmi bradrydzewski/dart:stable
-sudo docker rmi bradrydzewski/dart:dev
+docker rmi bradrydzewski/dart:stable
+docker rmi bradrydzewski/dart:dev
 
 # remove erlang images
-sudo docker rmi bradrydzewski/erlang:16B02
-sudo docker rmi bradrydzewski/erlang:16B01
-sudo docker rmi bradrydzewski/erlang:16B
-sudo docker rmi bradrydzewski/erlang				# KERL
+docker rmi bradrydzewski/erlang:16B02
+docker rmi bradrydzewski/erlang:16B01
+docker rmi bradrydzewski/erlang:16B
+docker rmi bradrydzewski/erlang				# KERL
 
 # remove gcc images
-sudo docker rmi bradrydzewski/gcc:4.8
-sudo docker rmi bradrydzewski/gcc:4.6
+docker rmi bradrydzewski/gcc:4.8
+docker rmi bradrydzewski/gcc:4.6
 
 # remove go images
-sudo docker rmi bradrydzewski/go:1.0
-sudo docker rmi bradrydzewski/go:1.1
-sudo docker rmi bradrydzewski/go:1.2
+docker rmi bradrydzewski/go:1.0
+docker rmi bradrydzewski/go:1.1
+docker rmi bradrydzewski/go:1.2
 
 # remove groovy images
 # TODO
 
 # remove haskell image
-sudo docker rmi bradrydzewski/haskell:7.4
+docker rmi bradrydzewski/haskell:7.4
 
 # remove java images
 # TODO
 
 # remove node images
-sudo docker rmi bradrydzewski/node:0.10
-sudo docker rmi bradrydzewski/node:0.8
-sudo docker rmi bradrydzewski/node					# NVM
+docker rmi bradrydzewski/node:0.10
+docker rmi bradrydzewski/node:0.8
+docker rmi bradrydzewski/node					# NVM
 
 # remove php images
-sudo docker rmi bradrydzewski/php:5.5
-sudo docker rmi bradrydzewski/php:5.4
-sudo docker rmi bradrydzewski/php:5.3
-sudo docker rmi bradrydzewski/php					# PHPENV
+docker rmi bradrydzewski/php:5.5
+docker rmi bradrydzewski/php:5.4
+docker rmi bradrydzewski/php:5.3
+docker rmi bradrydzewski/php					# PHPENV
 
 # remove python images
-sudo docker rmi bradrydzewski/python:2.7
-sudo docker rmi bradrydzewski/python:3.2
-sudo docker rmi bradrydzewski/python:3.3
-sudo docker rmi bradrydzewski/python:pypy
-sudo docker rmi bradrydzewski/python				# VIRTUALENV
+docker rmi bradrydzewski/python:2.7
+docker rmi bradrydzewski/python:3.2
+docker rmi bradrydzewski/python:3.3
+docker rmi bradrydzewski/python:pypy
+docker rmi bradrydzewski/python				# VIRTUALENV
 
 # remove ruby images
-sudo docker rmi bradrydzewski/ruby:2.0.0
-sudo docker rmi bradrydzewski/ruby:1.9.3
-sudo docker rmi bradrydzewski/ruby					# RBENV
+docker rmi bradrydzewski/ruby:2.0.0
+docker rmi bradrydzewski/ruby:1.9.3
+docker rmi bradrydzewski/ruby					# RBENV
 
 # remove scala image
-sudo docker rmi bradrydzewski/scala:2.9.3
-sudo docker rmi bradrydzewski/scala:2.10.3
+docker rmi bradrydzewski/scala:2.9.3
+docker rmi bradrydzewski/scala:2.10.3
 
 # remove the base image last
-sudo docker rmi bradrydzewski/base
+#docker rmi bradrydzewski/base
