3da7e43d61a4a34891e4395bda755024d62bed19
Brad Rydzewski <brad.rydzewski@gmail.com>
2013-12-02 15:31:09 -0700

    added scala and clojure docker images

--- builder/all.sh
+++ builder/all.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 set -e
 
+##################################################################################
 # Base Image
+
 sudo docker build -rm -t bradrydzewski/base builder/base/
 
+##################################################################################
 # Dart Images
 
 sudo docker build -rm -t bradrydzewski/dart     builder/dart/dart_stable/
 sudo docker build -rm -t bradrydzewski/dart:dev builder/dart/dart_dev/
 
+##################################################################################
 # Go Images
 
-sudo docker build -rm -t bradrydzewski/go:1.0 builder/golang/go_1.0/
-sudo docker build -rm -t bradrydzewski/go:1.1 builder/golang/go_1.1/
 sudo docker build -rm -t bradrydzewski/go:1.2 builder/golang/go_1.2/
+sudo docker build -rm -t bradrydzewski/go:1.1 builder/golang/go_1.1/
+#sudo docker build -rm -t bradrydzewski/go:1.0 builder/golang/go_1.0/
 
+##################################################################################
 # Haskell Images
 
 sudo docker build -rm -t bradrydzewski/haskell builder/haskell/haskell_7.4/
 
+##################################################################################
 # Node Images
 
 sudo docker build -rm -t bradrydzewski/node      builder/node/node/
 sudo docker build -rm -t bradrydzewski/node:0.10 builder/node/node_0.10/
 sudo docker build -rm -t bradrydzewski/node:0.8  builder/node/node_0.8/
-sudo docker build -rm -t bradrydzewski/node:0.6  builder/node/node_0.6/
+#sudo docker build -rm -t bradrydzewski/node:0.6  builder/node/node_0.6/
 
+##################################################################################
 # PHP Images
 
 sudo docker build -rm -t bradrydzewski/php     builder/php/php
 sudo docker build -rm -t bradrydzewski/php:5.5 builder/php/php_5.5
 sudo docker build -rm -t bradrydzewski/php:5.4 builder/php/php_5.4
-sudo docker build -rm -t bradrydzewski/php:5.3 builder/php/php_5.3
+#sudo docker build -rm -t bradrydzewski/php:5.3 builder/php/php_5.3
 
+##################################################################################
 # Python Images
 
 sudo docker build -rm -t bradrydzewski/python      builder/python/python/
@@ -42,24 +50,44 @@ sudo docker build -rm -t bradrydzewski/python:3.2  builder/python/python_3.2/
 sudo docker build -rm -t bradrydzewski/python:3.3  builder/python/python_3.3/
 sudo docker build -rm -t bradrydzewski/python:pypy builder/python/pypy/
 
+##################################################################################
 # Ruby Images
+
 sudo docker build -rm -t bradrydzewski/ruby        builder/ruby/ruby/
 sudo docker build -rm -t bradrydzewski/ruby:1.9.3  builder/ruby/ruby_1.9.3/
 sudo docker build -rm -t bradrydzewski/ruby:2.0.0  builder/ruby/ruby_2.0.0/
 
+##################################################################################
 # Java Images
 
+sudo docker build -rm -t bradrydzewski/java:oraclejdk8  builder/java/java_oraclejdk7/
+sudo docker build -rm -t bradrydzewski/java:oraclejdk7  builder/java/java_oraclejdk7/
+sudo docker build -rm -t bradrydzewski/java:openjdk7  builder/java/java_openjdk7/
+sudo docker build -rm -t bradrydzewski/java:openjdk6  builder/java/java_openjdk6/
 
+##################################################################################
 # Scala Images
 
+sudo docker build -rm -t bradrydzewski/scala:2.10.3  builder/scala/scala_2.10.3/
+sudo docker build -rm -t bradrydzewski/scala:2.9.3   builder/scala/scala_2.9.3/
 
+##################################################################################
 # Groovy Images
 
 
+
+##################################################################################
+# Clojure Images
+
+sudo docker build -rm -t bradrydzewski/lein builder/clojure/lein/
+
+##################################################################################
 # GCC Images
+
 #sudo docker build -rm -t bradrydzewski/gcc:4.6  builder/gcc/gcc_4.6/
 #sudo docker build -rm -t bradrydzewski/gcc:4.8  builder/gcc/gcc_4.8/
 
+##################################################################################
 # Erlang Images
 
 sudo docker build -rm -t bradrydzewski/erlang builder/erlang/erlang/
