2c4bec9b2292a4ae01c100f0b07bb18a7ba26528
Brad Rydzewski <brad.rydzewski@gmail.com>
2013-12-02 13:11:20 -0700

    updated to Go 1.2 updated memcache version adding initial ruby image

--- builder/all.sh
+++ builder/all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Base Image
 sudo docker build -rm -t bradrydzewski/base builder/base/
@@ -7,22 +8,6 @@ sudo docker build -rm -t bradrydzewski/base builder/base/
 
 sudo docker build -rm -t bradrydzewski/dart     builder/dart/dart_stable/
 sudo docker build -rm -t bradrydzewski/dart:dev builder/dart/dart_dev/
-
-# Erlang Images
-
-sudo docker build -rm -t bradrydzewski/erlang builder/erlang/erlang/
-sudo docker build -rm -t bradrydzewski/erlang:R16B02 builder/erlang/erlang_R16B02/
-sudo docker build -rm -t bradrydzewski/erlang:R16B01 builder/erlang/erlang_R16B01/
-sudo docker build -rm -t bradrydzewski/erlang:R16B   builder/erlang/erlang_R16B/
-sudo docker build -rm -t bradrydzewski/erlang:R15B03 builder/erlang/erlang_R15B03/
-sudo docker build -rm -t bradrydzewski/erlang:R15B02 builder/erlang/erlang_R15B02/
-sudo docker build -rm -t bradrydzewski/erlang:R15B01 builder/erlang/erlang_R15B01/
-sudo docker build -rm -t bradrydzewski/erlang:R15B   builder/erlang/erlang_R15B/
-sudo docker build -rm -t bradrydzewski/erlang:R14B04 builder/erlang/erlang_R14B04/
-sudo docker build -rm -t bradrydzewski/erlang:R14B03 builder/erlang/erlang_R14B03/
-sudo docker build -rm -t bradrydzewski/erlang:R14B02 builder/erlang/erlang_R14B02/
-sudo docker build -rm -t bradrydzewski/erlang:R14B01 builder/erlang/erlang_R14B01/
-sudo docker build -rm -t bradrydzewski/erlang:R14A   builder/erlang/erlang_R14A/
 
 # Go Images
 
@@ -57,5 +42,36 @@ sudo docker build -rm -t bradrydzewski/python:3.2  builder/python/python_3.2/
 sudo docker build -rm -t bradrydzewski/python:3.3  builder/python/python_3.3/
 sudo docker build -rm -t bradrydzewski/python:pypy builder/python/pypy/
 
- # Ruby Images
+# Ruby Images
+sudo docker build -rm -t bradrydzewski/ruby        builder/ruby/ruby/
+sudo docker build -rm -t bradrydzewski/ruby:1.9.3  builder/ruby/ruby_1.9.3/
+sudo docker build -rm -t bradrydzewski/ruby:2.0.0  builder/ruby/gruby_2.0.0/
 
+# Java Images
+
+
+# Scala Images
+
+
+# Groovy Images
+
+
+# GCC Images
+#sudo docker build -rm -t bradrydzewski/gcc:4.6  builder/gcc/gcc_4.6/
+#sudo docker build -rm -t bradrydzewski/gcc:4.8  builder/gcc/gcc_4.8/
+
+# Erlang Images
+
+sudo docker build -rm -t bradrydzewski/erlang builder/erlang/erlang/
+sudo docker build -rm -t bradrydzewski/erlang:R16B02 builder/erlang/erlang_R16B02/
+sudo docker build -rm -t bradrydzewski/erlang:R16B01 builder/erlang/erlang_R16B01/
+sudo docker build -rm -t bradrydzewski/erlang:R16B   builder/erlang/erlang_R16B/
+sudo docker build -rm -t bradrydzewski/erlang:R15B03 builder/erlang/erlang_R15B03/
+sudo docker build -rm -t bradrydzewski/erlang:R15B02 builder/erlang/erlang_R15B02/
+sudo docker build -rm -t bradrydzewski/erlang:R15B01 builder/erlang/erlang_R15B01/
+sudo docker build -rm -t bradrydzewski/erlang:R15B   builder/erlang/erlang_R15B/
+sudo docker build -rm -t bradrydzewski/erlang:R14B04 builder/erlang/erlang_R14B04/
+sudo docker build -rm -t bradrydzewski/erlang:R14B03 builder/erlang/erlang_R14B03/
+sudo docker build -rm -t bradrydzewski/erlang:R14B02 builder/erlang/erlang_R14B02/
+sudo docker build -rm -t bradrydzewski/erlang:R14B01 builder/erlang/erlang_R14B01/
+#sudo docker build -rm -t bradrydzewski/erlang:R14A   builder/erlang/erlang_R14A/
