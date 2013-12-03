648e4fe913e61326ff78e69a45c16d8d97475d58
Brad Rydzewski <brad.rydzewski@gmail.com>
2013-12-03 15:07:21 -0700

    formatted go rc scripts

--- builder/golang/go_1.1/golang.sh
+++ builder/golang/go_1.1/golang.sh
@@ -2,15 +2,3 @@
 export GOROOT=/usr/local/go
 export GOPATH=/var/cache/drone
 export PATH=$PATH:$GOROOT/bin
-
-[ -d $GOPATH/src ] || mkdir -p $GOPATH/src
-[ -d $GOPATH/pkg ] || mkdir -p $GOPATH/pkg
-[ -d $GOPATH/bin ] || mkdir -p $GOPATH/bin
-
-# enable ruby support via rbenv
-
-# enable python support via virtualenv
-
-# enable nodejs support via nvm
-
-# enable java support via JAVA_HOME, MAVEN_HOME, etc
