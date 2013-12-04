3590d935c4a2c62381131bc66a269d5688f0cc33
Brad Rydzewski <brad.rydzewski@gmail.com>
2013-12-03 18:37:05 -0700

    moved NVM to the base image

--- builder/base/scripts/nodejs.sh
+++ builder/base/scripts/nodejs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # add the ubuntu PPA for Node
-sudo apt-get install software-properties-common
-sudo add-apt-repository --yes ppa:chris-lea/node.js
-sudo apt-get -qq update
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository --yes ppa:chris-lea/node.js
+#sudo apt-get -qq update
 
 # install latest stable version of nodejs
-sudo apt-get -y install nodejs
+#sudo apt-get -y install nodejs
+
+git clone git://github.com/creationix/nvm.git /home/ubuntu/nvm
+. /home/ubuntu/nvm/nvm.sh
+nvm install v0.10.22
